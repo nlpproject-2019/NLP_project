b95514e62f62b1624d99afc0d0c1074526d9935f
rohi <rohi040404@gmail.com>
2019-06-08 17:12:39 +0900

    feat: Rename

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from konlpy.tag import Okt
 from collections import Counter
 import os
@@ -82,3 +83,50 @@ def PreprocessFiles(file_dir):
     #print(len(x.keys()))
 
     return x
+=======
+import math
+import time
+import numpy as np
+
+from collections import Counter
+from konlpy.tag import Okt 
+okt=Okt()
+
+# (전처리) 길이가 2이상인 명사만 고르기
+def get_nouns(text):
+    nouns = okt.nouns(text)
+    nouns = [word for word in nouns if len(word) > 1]
+    return nouns
+
+wordbox = []
+
+file = open("input/10.txt", 'r', encoding='UTF8')
+doc = file.read()
+file.close()
+token = get_nouns(doc)
+wordbox.append(Counter(token))
+
+file = open("input/20.txt", 'r', encoding='UTF8')
+doc = file.read()
+file.close()
+token = get_nouns(doc)
+wordbox.append(Counter(token))
+
+file = open("input/30.txt", 'r', encoding='UTF8')
+doc = file.read()
+file.close()
+token = get_nouns(doc)
+wordbox.append(Counter(token))
+
+file = open("input/40.txt", 'r', encoding='UTF8')
+doc = file.read()
+file.close()
+token = get_nouns(doc)
+wordbox.append(Counter(token))
+
+file = open("input/50.txt", 'r', encoding='UTF8')
+doc = file.read()
+file.close()
+token = get_nouns(doc)
+wordbox.append(Counter(token))
+>>>>>>> weighting
